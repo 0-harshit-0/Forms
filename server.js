349ef3d585b71a64988817a0cd64e7f956ba0de9
harshit singhal <harshitw3b@gmail.com>
2021-11-25 01:18:25 +0530

    changes

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 let express = require("express");
 let app = new express();
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:1234qwer@cluster0.gzwoo.mongodb.net/mydb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const collection = client.db("mydb").collection("temp");
 
-
-app.use(express.static("views"));
+app.use(express.urlencoded({
+  extended: true
+}))
 app.use(express.static("src"));
 app.use(express.static("assets"));
 
 app.get("/", (req, res)=> {
-	res.sendFile("./index.html");
-})
+	res.sendFile(__dirname+"/views/index.html");
+});
+
+app.post("/submit", (req, res)=> {
+	//console.log(req.body)
+	res.sendFile(__dirname+"/views/submit.html");
+
+	//+++mongo_here+++
+	client.connect(async err => {
+		var myobj = { name: req.body.fn, dob: req.body.dob, gender: req.body.gen };
+		await collection.insertOne(myobj, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			client.close();
+		});
+	});
+});
 
 app.listen(8080);

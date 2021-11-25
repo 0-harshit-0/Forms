92f3590e01da15393f6142f03014a0bc4f398db4
harshit singhal <harshitw3b@gmail.com>
2021-11-25 23:35:49 +0530

    changes

--- src/script.js
+++ src/script.js
@@ -3,3 +3,8 @@
 		z.setAttribute("contenteditable", "true");
 	});
 }*/
+
+let bts = document.querySelectorAll(".btns");
+bts[1].onclick = () => {
+	document.querySelectorAll("input").forEach(z=> {z.value=""});
+}
